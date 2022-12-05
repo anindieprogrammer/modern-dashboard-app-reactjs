2653eea687bf61899768e61dded6bb1510ea8af1
Shahrukhkhan Pathan <anindieprogrammer@gmail.com>
2022-12-05 17:14:47 +0530

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import { createRoot } from "react-dom/client";
+
+import "./index.css";
+import App from "./App";
+import { ContextProvider } from "./contexts/ContextProvider";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
+  <ContextProvider>
+    <App />
+  </ContextProvider>
+);
